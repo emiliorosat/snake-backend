15579ccc7ea350f3ee868fe48ccca77d1ebb7048
Emilio Rosa <me@emiliort.com>
2020-11-20 23:10:46 -0400

    prueba Dependes

--- main.py
+++ main.py
@@ -53,6 +53,10 @@ def encodeToken(user: Usuario):
         "Nombre": user.Nombre,
         "Email": user.Email
     }}, jwtSecreto)
+
+def verifyFetch(token):
+    return True
+
 #--------------------------
 
 #class Token(BaseModel):
@@ -159,7 +163,8 @@ def Info(Id:int = Depends(get_current_active_user)):
 
 #-------------------Paciente--------------
 @app.get("/api/patients", tags=["Patient"])
-def Patients(uid: int):
+def Patients(uid: int, token = Depends(verifyFetch)):
+    print(token)
     conexion = sqlite3.connect(ruta)
     datos = conexion.cursor()
 
